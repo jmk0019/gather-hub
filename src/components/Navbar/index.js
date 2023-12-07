fbce0445943f34ad03e3366f4c63ec3e27268b2c
akhilesh423 <akhilakhilesh423@gmail.com>
2023-12-08 03:21:59 +0530

    solution

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+
+import "./index.css"
+
+const Navbar = () => (
+    <nav>
+        <h1 className="gather-hub">Gather Hub</h1>
+        <ul>
+            <li>Sign up</li>
+            <li>Support</li>
+        </ul>
+    </nav>
+
+)
+
+
+
+export default Navbar
